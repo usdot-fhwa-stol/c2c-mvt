95e60ef03525ba42cb788d84a5326a5fa1996371
dan-du-car <dan.du@leidos.com>
2025-03-05 17:53:16 -0500

    init

---
+++ src/main/java/usdot/fhwa/stol/c2cri/c2cri_mvt/C2criMvtApplication.java
@@ -0,0 +1,13 @@
+package usdot.fhwa.stol.c2cri.c2cri_mvt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C2criMvtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C2criMvtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/usdot/fhwa/stol/c2cri/c2cri_mvt/C2criMvtApplicationTests.java
@@ -0,0 +1,13 @@
+package usdot.fhwa.stol.c2cri.c2cri_mvt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C2criMvtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
