5b4c512c71eaf3744e0537a43f4344ce13916d20
cherneysp <aaron.cherney@synesis-partners.com>
2025-05-06 15:10:23 -0500

    Added license header and Eric as author

--- src/test/java/usdot/fhwa/stol/c2c/c2c_mvt/decoders/JsonDecoderTest.java
+++ src/test/java/usdot/fhwa/stol/c2c/c2c_mvt/decoders/JsonDecoderTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2025 LEIDOS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package usdot.fhwa.stol.c2c.c2c_mvt.decoders;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -11,6 +26,11 @@ import java.util.ArrayList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+/**
+ * Unit tests for JsonDecoder
+ * 
+ * @author Eric Chen
+ */
 class JsonDecoderTest {
 
     private JsonDecoder jsonDecoder;
